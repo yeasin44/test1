8c0f8d592ce293d3385491173d4284adaac689a4
hmathir <hmathir@gmail.com>
2022-09-11 23:28:40 +0600

    Fixed

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const displayCategory = async (menus) => {
              <div id="res-menu">
              <li  onclick="loadAllNews('${
                menu.category_id
-             }'), toggleSpinner(true)" class="md:mr-4  md:ml-7 hover:bg-gray-600 p-2  cursor-pointer text-white">${
+             }', '${menu.category_name}'), toggleSpinner(true)" class="md:mr-4  md:ml-7 hover:bg-gray-600 p-2  cursor-pointer text-white">${
       menu.category_name ? menu.category_name : "No Data found"
     }</li></div>
 
@@ -31,20 +31,28 @@ const displayCategory = async (menus) => {
 };
 
 // .............................Display news...............................
-const loadAllNews = async (category_id) => {
+const loadAllNews = async (category_id, category) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/news/category/${category_id}`
   );
   const data = await response.json();
-  displayNews(data.data);
+  displayNews(data.data, category);
   // console.log(data.data);
 };
-const displayNews = async (allNews) => {
+
+const displayNews = async (allNews, category) => {
+  console.log(allNews);
+  const item = document.getElementById('item-found');
+  item.innerText = allNews.length;
+  const cate = document.getElementById('item-category');
+  cate.innerText = category;
   // const data = await loadAllNews();
   const newsContainer = document.getElementById("news-container");
   newsContainer.textContent = "";
   // spinner.classList.add("hidden");
-  allNews.forEach((news) => {
+  const sortedAllNews = allNews.sort((a,b) => b.total_view - a.total_view);
+  sortedAllNews.forEach((news) => {
+    // console.log(news._id);
     const newsDiv = document.createElement("div");
 
     newsDiv.innerHTML = `
@@ -95,14 +103,10 @@ const displayNews = async (allNews) => {
                 </div>
             </div>
             <div class="mt-24" id="icon">
-              <label id="icon2" for="my-modal-3" onclick="showModal('${
-                news.details
-              }')" class=" modal-button"><i class="fa-solid fa-arrow-right text-primary "></i></label>
-            </div>
+              <label onclick='showModal("${news._id}")' id="icon2" for="my-modal-3" class=" modal-button"><i class="fa-solid fa-arrow-right text-primary "></i></label>
+            </div> 
             
             </div>
-           
-            
           </div>
         </div>
              
@@ -113,12 +117,36 @@ const displayNews = async (allNews) => {
   });
   toggleSpinner(false);
 };
+loadAllNews('08', 'All News');
 // loadAllNews();
 loadAllCategory();
 // ............................Modal...........................
-const showModal = (news) => {
-  console.log(news);
+const showModal = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/${id}`);
+    const data = await response.json();
+    displayModalData(data.data[0]);
 };
+
+//DISPLAY MODAL DATA
+const displayModalData = (news) => {
+  const parentModal = document.getElementById('show-modal');
+  parentModal.innerHTML = `
+  <img src="${news?.image_url ? news.image_url : 'No Image Found'}" alt="">
+  <h3 class="text-3xl font-bold mt-3">${news?.title ? news.title : 'No Title Found'}</h3>
+  <div class="flex justify-between text-teal-500 font-bold mt-5">
+      <p><kbd class="kbd kbd-sm">Author Name: ${news?.author.name ? news.author.name : 'No Data Found'}</kbd></p>
+      <p><kbd class="kbd kbd-sm">${news?.author?.published_date ? news.author.published_date : 'No Data Found'}</kbd></p>
+  </div>
+  <p class="mt-5"><kbd class="kbd kbd-sm">Details:</kbd></p>
+  <p class="py-4"><kbd class="kbd kbd-sm">${news?.details ? news.details : 'No Details Found'}</kbd></p>
+
+  <div class="flex justify-between text-purple-500 font-bold">
+      <p><kbd class="kbd kbd-sm">Total View: ${news?.total_view ? news.total_view : 'No Data Found'}</kbd></p>
+      <p><kbd class="kbd kbd-sm">Rating: ${news?.rating?.number ? news.rating.number : 'No Data Found'}</kbd></p>
+  </div>
+`;
+}
+
 const toggleSpinner = (isLoading) => {
   loadSpinner = document.getElementById("spinner");
   if (isLoading) {
